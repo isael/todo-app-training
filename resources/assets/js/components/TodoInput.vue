82b25b96cc64f1ac9a56868d998e2d0018bac4ae
isael <isael191@gmail.com>
2017-12-04 00:15:47 -0600

    Se agregan los archivos a git TodoInput y TodoItem

---
+++ resources/assets/js/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<template>    
+    <form method= "POST" v-on:submit.prevent="$emit('addTodo')">
+        <div class="box">
+            <div class="field is-grouped">
+                <p class="control is-expanded">
+                    <input class="input" type="text" placeholder="Nuevo recordatorio" :value="todoItemText" @input="changeText">
+                </p>
+                <p class="control">
+                    <a class="button is-info" @click="$emit('addTodo')">
+                        Agregar
+                    </a>
+                </p>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        props:['todoItemText'], //Propiedad que será modificada desde el vue Todo
+        data () {
+            return {
+                todoItemInputText: ''
+            }
+        },
+        methods: {
+            changeText(event){
+                this.todoItemInputText = event.target.value;    //Detecta los cambios de Todo y actualiza
+                this.$emit('changeText',this.todoItemInputText) //Actualiza todoItemText del Todo
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<td class="is-fullwidth" style="cursor: pointer" :class="{ 'is-done': done }" @click="$emit('toggleDone',id)">
+	        {{ text }}
+	    </td>
+	    <td class="is-narrow">
+	        <a class="button is-danger is-small" @click.prevent="$emit('removeTodo',id)">Eliminar</a>
+	    </td>
+    </div>
+</template>
+
+<script>
+	export default{
+		props:{
+			text: {
+				type: String,				
+				required: true,
+			},
+			id: {
+				type: Number,
+				required: true,
+			},
+			done: {
+				type: Boolean,
+				required: true,
+			}
+		}
+	}
+</script>
